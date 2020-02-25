e2ca81cc7d72194d3b57a289fb09cf76b69fa381
Thanh Nguyen <tienthanhnt123@gmail.com>
2020-02-25 21:33:11 +0700

    Fix passenger list bug

--- flights/views.py
+++ flights/views.py
@@ -18,7 +18,7 @@ def flight(request, flight_id):
         raise Http404("Flight does not exist")
     context = {
         "flight": flight,
-        "non_passengers": flight.passengers.all(),
+        "passengers": flight.passengers.all(),
         "non_passengers": Passenger.objects.exclude(flights=flight).all()
     }
     return render(request, "flights/flight.html", context)
